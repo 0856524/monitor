b68463c218e19be0363718ffd59ab51400a2233e
LEE PEI HSUAN <jamie7jamie@gmail.com>
2020-09-22 16:37:55 +0800

    change the way to monitor CPU and memory

--- MonitorInstances.py
+++ MonitorInstances.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 from subprocess import check_output
 import socket
 import requests
-import logging
+import psutil
 
 ips = check_output(['hostname', '--all-ip-addresses'])
 ip = str(ips.decode())
@@ -17,25 +17,32 @@ print("Instances's IP: " + ip)
 init_flag = 0
 start_time = time.time()
 while True:
-    CPU_Pctn = os.popen("""top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1""").readline()
+    '''CPU_Pctn = os.popen("""top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1""").readline()
     CPU_Pct = CPU_Pctn.split('\n')[0]
 
-    # print results
-    mem = str(os.popen('free -t -m').readlines())
-
+    print("Total mem:" + Total_mem + " Mb")
+    print("Used mem:" + Used_mem + " Mb")
+    print('Mem usage in percent:' + str(mem_Pct) + "%")mem = str(os.popen('free -t -m').readlines())
     get_mem = mem.split()
     Total_mem = get_mem[8]
     Used_mem = get_mem[9]
     mem_Pct = (float(Used_mem)/float(Total_mem))*100
-    mem_Pct = round(mem_Pct, 2)
+    mem_Pct = round(mem_Pct, 2)'''
+
+    CPU_Pct = psutil.cpu_percent()
+    Total_mem = psutil.virtual_memory().total
+    Used_mem = psutil.virtual_memory().used
+    mem_Pct = (float(Used_mem)/float(Total_mem))*100
+    mem_Pct = round(mem_Pct, 2)    
+    
     print("*******************************")
     print("CPU Usage = " + str(CPU_Pct) + "%")
-    print("Total mem:" + Total_mem + " Mb")
-    print("Used mem:" + Used_mem + " Mb")
+    print("Total mem:" + str(Total_mem/(1024*1024)) + " Mb")
+    print("Used mem:" + str(Used_mem/(1024*1024)) + " Mb")
     print('Mem usage in percent:' + str(mem_Pct) + "%")
     print("*******************************")
 
-    # time.sleep(1)
+    time.sleep(1)
     '''# print(type(CPU_Pct))
     payload = "{ip}, {CPU_Pct}".format(ip=ip, CPU_Pct=CPU_Pct)
     r = requests.post('http://192.168.122.190:9999/Instances', data=payload)
