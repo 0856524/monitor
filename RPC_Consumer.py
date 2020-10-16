18289e6e4b81170c1140680420865b5832a30a62
LEE PEI HSUAN <jamie7jamie@gmail.com>
2020-10-16 22:37:34 +0800

    modify check port

--- RPC_Consumer.py
+++ RPC_Consumer.py
@@ -3,17 +3,19 @@ import json
 import random
 import requests
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = sock.connect_ex(('127.0.0.1',8080))
 while result != 0:
-    pass
+    result = sock.connect_ex(('127.0.0.1',8080))
     #if result == 0:
         #print("Port is open")
         #break
     #else:
         #print("Port is not open")
         #continue
+
 
 credentials = pika.PlainCredentials("admin","0000")
 #connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.24.4.184',credentials=credentials))
