28a3aff4a91144525b49bd0c50ad69c27b4fbc73
LEE PEI HSUAN <jamie7jamie@gmail.com>
2020-12-02 16:22:18 +0800

    debug

--- triggle_delete.sh
+++ triggle_delete.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ssh -i cloud.key ubuntu@$1 "/home/ubuntu/monitor/monitor-instance/delete.sh && exit"
+ssh -i cloud.key ubuntu@$1 "/home/ubuntu/monitor/monitor-instance/delete.sh && exit"
