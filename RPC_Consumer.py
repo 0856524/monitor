7542c5027fa9a8b50393f8d63ebe46ad9da07d58
LEE PEI HSUAN <jamie7jamie@gmail.com>
2020-12-15 18:10:14 +0800

    debug

--- RPC_Consumer.py
+++ RPC_Consumer.py
@@ -33,7 +33,7 @@ while True:
 print(ins_no)
 lb_ip = lb_ip + str(int(200+((int(ins_no)-1)/max_ins)+1))'''
 
-lb_ip = '172.24.4.' + str(int(200+int(sys.argv[1])))
+lb_ip = '172.24.4.' + str(int(10*int(sys.argv[1]))+5)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = sock.connect_ex(('127.0.0.1',8080))
